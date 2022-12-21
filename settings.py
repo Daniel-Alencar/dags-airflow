ec0a10f56f47fe2958c97aa7d53e343c7d2ba604
Daniel Alencar <danielalencar746@gmail.com>
2022-12-21 11:31:04 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
-absolute_path = "/home/engenheiro/airflow/dags/"
+absolute_path = ""
 
 vehicles_to_search_path = f"{absolute_path}json/vehicles_to_search.json"
 vehicles_with_price_path = f"{absolute_path}json/vehicles_with_price.json"
 modelo_atual_path = f"{absolute_path}json/modelo_atual.json"
 
 verbose = True
-headless = True
+headless = False
 
 # NÃO PODEM MUDAR
 number_of_computers = 5
@@ -14,7 +14,7 @@ number_of_years = 3
 # PODEM MUDAR
 
 # Começa em 0
-computer_id = 1
+computer_id = 0
 mini_batch = 8
 
 meses = [
