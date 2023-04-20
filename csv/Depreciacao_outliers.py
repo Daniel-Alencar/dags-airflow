8650c3370292985b097fa1ae46fe985fa32b4568
Daniel-Alencar <danielalencar746@gmail.com>
2023-04-19 23:30:27 -0300

    Refatoramento

--- csv/Depreciacao_outliers.py
+++ csv/Depreciacao_outliers.py
@@ -7,13 +7,13 @@ path = "csv/Modelos/Modelo1 comprimido (limitado)"
 extension = ".csv"
 path_to_dataframe = path + extension
 
-marcas = [
-  "Audi",
-  "BMW",
-  "CAOA Chery"
-]
-
 new_data = pd.DataFrame(columns=[
+  "Marca",
+  "Modelo",
+  "Ano-modelo",
+  "Amplitude"
+])
+new_data_for_brands = pd.DataFrame(columns=[
   "Marca",
   "Modelo",
   "Ano-modelo",
@@ -21,8 +21,14 @@ new_data = pd.DataFrame(columns=[
 ])
 
 
+marcas = [
+  "Audi",
+  "BMW",
+  "CAOA Chery"
+]
+
+# Calculando depreciação nos outliers
 for i in range(len(marcas)):
-  
   data = pd.read_csv(path_to_dataframe)
   data = data.loc[data['Marca'] == marcas[i]]
   index = 0
@@ -38,7 +44,7 @@ for i in range(len(marcas)):
     porcentagem = (value1 - value2) / value1
 
     vetor = [
-      marcas[i],
+      data.iloc[index]["Marca"],
       data.iloc[index]["Modelo"],
       data.iloc[index]["Ano-modelo"],
       porcentagem
@@ -47,3 +53,41 @@ for i in range(len(marcas)):
     index += 1
 
 print(new_data)
+new_data.to_csv("csv/depreciacao_outliers.csv", index=False)
+
+
+
+# Calculando depreciação nos outros veículos
+data = pd.read_csv(path_to_dataframe)
+
+index = 0
+while True:
+  if(index >= len(data.index)):
+    break
+
+  marca = data.iloc[index]["Marca"]
+
+  marca_diferente = True
+  for item in marcas:
+    if(marca == item):
+      marca_diferente = False
+
+  if(marca_diferente):
+    value1 = data.iloc[index]["Valores preditos"]
+    index += 11
+    value2 = data.iloc[index]["Valores preditos"]
+
+    porcentagem = (value1 - value2) / value1
+
+    vetor = [
+      data.iloc[index]["Marca"],
+      data.iloc[index]["Modelo"],
+      data.iloc[index]["Ano-modelo"],
+      porcentagem
+    ]
+    new_data_for_brands.loc[len(new_data_for_brands)] = vetor
+  
+  index += 1
+
+print(new_data_for_brands)
+new_data_for_brands.to_csv("csv/depreciacao_marcas.csv", index=False)
